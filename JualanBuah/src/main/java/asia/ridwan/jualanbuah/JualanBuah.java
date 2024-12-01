8df2611c0344ef1ee72a8702186fbb026e7e95d3
KaizoWan <156559854+KaizoWan@users.noreply.github.com>
2024-12-02 03:35:41 +0700

    Add files via upload

---
+++ JualanBuah/src/main/java/asia/ridwan/jualanbuah/JualanBuah.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package asia.ridwan.jualanbuah;
+
+/**
+ *
+ * @author ridwan
+ */
+public class JualanBuah {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
